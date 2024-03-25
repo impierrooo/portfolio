c2947873562b50a6ec1100158b8fb6efc5ab2cda
Pierre Montaret <pierre.montaret@epitech.eu>
2024-03-25 11:00:48 +0100

    [ADD] page contact

--- source/js/contact.js
+++ source/js/contact.js
@@ -1,25 +1,31 @@
 /* ---------- Contact ---------- */
 
 function contact() {
-	var nom = document.getElementById("nom").value;
-    var prénom = document.getElementById("prénom").value;
-	var email = document.getElementById("email").value;
+	var nom = document.getElementById("name").value;
+    var email = document.getElementById("mail").value;
 	var message = document.getElementById("message").value;
 	
 	if (nom == '') {
-		Alert();
+		my_sucess();
+		document.getElementById("alert_fill").style.transform = "scale(1)";
+		document.getElementById("alert_fill_background").style.transform = "scale(1)";
+		setTimeout(my_fill, 5000);
 		
-	} else if (prénom == '') {
-		Alert();
-		
+
 	} else if (email == '') {
-		Alert();
+		my_sucess();
+		document.getElementById("alert_fill").style.transform = "scale(1)";
+		document.getElementById("alert_fill_background").style.transform = "scale(1)";
+		setTimeout(my_fill, 5000);
 
     } else if (message == '') {
-		Alert();
-	
+		my_sucess();
+		document.getElementById("alert_fill").style.transform = "scale(1)";
+		document.getElementById("alert_fill_background").style.transform = "scale(1)";
+		setTimeout(my_fill, 5000);
 	} else {
 
+		my_fill();
 		var url = "https://discord.com/api/webhooks/1013789946345173022/wJ87qxQlb32z9Nx3lYl4F0ZAX4peXFB8iYIH2VN_4DprlRrwzvKt2MObDuvfCxEwpkXt";
 		var request = new XMLHttpRequest();
 		request.open("POST", url);
@@ -30,15 +36,11 @@ function contact() {
 			"color": 0xDA7426,
 			"fields": [
 				{
-					"name": "Nom :",
+					"name": "Name :",
 					"value": nom,
 				},
-                {
-					"name": "Prénom :",
-					"value": prénom,
-				},
 				{
-					"name": "Email :",
+					"name": "E-mail :",
 					"value": email,
 				},
 				{
@@ -55,9 +57,22 @@ function contact() {
     
 		request.send(JSON.stringify(params)); 
 		
-		document.getElementById("nom").value = "";
-        document.getElementById("prénom").value = "";
-		document.getElementById("email").value = "";
+		document.getElementById("name").value = "";
+		document.getElementById("mail").value = "";
 		document.getElementById("message").value = "";
+
+		document.getElementById("alert_sucess").style.transform = "scale(1)";
+		document.getElementById("alert_sucess_background").style.transform = "scale(1)";
+		setTimeout(my_sucess, 5000);
 	}
+}
+
+function my_fill() {
+    document.getElementById("alert_fill").style.transform = "scale(0)";
+    document.getElementById("alert_fill_background").style.transform = "scale(0)";
+}
+
+function my_sucess() {
+    document.getElementById("alert_sucess").style.transform = "scale(0)";
+    document.getElementById("alert_sucess_background").style.transform = "scale(0)";
 }
--- source/js/langue.js
+++ source/js/langue.js
@@ -1,5 +1,8 @@
 function changement_langue(){
     var langue = localStorage.getItem('langue');
+    var page = localStorage.getItem('page');
+    var number = parseInt(page);
+    console.log(page);
 
 	if (langue === 'FR') {
         localStorage.setItem('langue', 'EN');
@@ -10,9 +13,19 @@ function changement_langue(){
         document.getElementById('modal3').innerHTML = "ABOUT";
 
         document.getElementById('top1').innerHTML = "AVAILABLE FOR AN INTERNSHIP";
-
+        
         document.getElementById('bottom1').innerHTML = "© 2024 Impierrooo • All rights reserved";
         document.getElementById('bottom2').innerHTML = "Web design and development";
+        
+        if (number === 4) {
+            document.getElementById('name_label').innerHTML = "<svg class='form_svg' viewBox='0 0 24 24'><path d='M7.5 6.5C7.5 8.981 9.519 11 12 11s4.5-2.019 4.5-4.5S14.481 2 12 2 7.5 4.019 7.5 6.5zM20 21h1v-1c0-3.859-3.141-7-7-7h-4c-3.86 0-7 3.141-7 7v1h17z'></path></svg> Name";
+            document.getElementById('contact_button').innerHTML = "SEND";
+
+            document.getElementById('alert_fill').innerHTML = "Please fill in all fields <img src='./source/assets/images/error.png' class='alert_icon'>";
+            document.getElementById('alert_fill_background').style.width = "210.73px";
+            document.getElementById('alert_sucess').innerHTML = "Message sent <img src='./source/assets/images/succes.png' class='alert_icon'>";
+            document.getElementById('alert_sucess_background').style.width = "159.78px";
+        }
 	} else {
         localStorage.setItem('langue', 'FR');
         document.getElementById("langue").innerHTML = "EN";
@@ -22,8 +35,18 @@ function changement_langue(){
         document.getElementById('modal3').innerHTML = "À PROPOS";
 
         document.getElementById('top1').innerHTML = "DISPONIBLE POUR UN STAGE";
-
+        
         document.getElementById('bottom1').innerHTML = "© 2024 Impierrooo • Tous droits réservés";
         document.getElementById('bottom2').innerHTML = "Conception et développement web";
+
+        if (number === 4) {
+            document.getElementById('name_label').innerHTML = "<svg class='form_svg' viewBox='0 0 24 24'><path d='M7.5 6.5C7.5 8.981 9.519 11 12 11s4.5-2.019 4.5-4.5S14.481 2 12 2 7.5 4.019 7.5 6.5zM20 21h1v-1c0-3.859-3.141-7-7-7h-4c-3.86 0-7 3.141-7 7v1h17z'></path></svg> Nom";
+            document.getElementById('contact_button').innerHTML = "ENVOYER";
+
+            document.getElementById('alert_fill').innerHTML = "Veuillez compléter tous les champs <img src='./source/assets/images/error.png' class='alert_icon'>";
+            document.getElementById('alert_fill_background').style.width = "315.66px";
+            document.getElementById('alert_sucess').innerHTML = "Message envoyé <img src='./source/assets/images/succes.png' class='alert_icon'>";
+            document.getElementById('alert_sucess_background').style.width = "180.83px";
+        }
     }
 }

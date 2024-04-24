f0121edcff92909f198e8221c890fb95f766bfd7
Pierre Montaret <pierre.montaret@epitech.eu>
2024-04-24 17:02:46 +0200

    [UPDATE] all pages

--- source/js/langue.js
+++ source/js/langue.js
@@ -15,7 +15,7 @@ function changement_langue(){
         document.getElementById('top1').innerHTML = "AVAILABLE FOR AN INTERNSHIP";
         
         document.getElementById('bottom1').innerHTML = "© 2024 Impierrooo • All rights reserved";
-        document.getElementById('bottom2').innerHTML = "Web design and development";
+        document.getElementById('bottom2').innerHTML = "Creative and original developer";
         
         if (number === 4) {
             document.getElementById('name_label').innerHTML = "<svg class='form_svg' viewBox='0 0 24 24'><path d='M7.5 6.5C7.5 8.981 9.519 11 12 11s4.5-2.019 4.5-4.5S14.481 2 12 2 7.5 4.019 7.5 6.5zM20 21h1v-1c0-3.859-3.141-7-7-7h-4c-3.86 0-7 3.141-7 7v1h17z'></path></svg> Name";
@@ -37,7 +37,7 @@ function changement_langue(){
         document.getElementById('top1').innerHTML = "DISPONIBLE POUR UN STAGE";
         
         document.getElementById('bottom1').innerHTML = "© 2024 Impierrooo • Tous droits réservés";
-        document.getElementById('bottom2').innerHTML = "Conception et développement web";
+        document.getElementById('bottom2').innerHTML = "Développeur créatif et original";
 
         if (number === 4) {
             document.getElementById('name_label').innerHTML = "<svg class='form_svg' viewBox='0 0 24 24'><path d='M7.5 6.5C7.5 8.981 9.519 11 12 11s4.5-2.019 4.5-4.5S14.481 2 12 2 7.5 4.019 7.5 6.5zM20 21h1v-1c0-3.859-3.141-7-7-7h-4c-3.86 0-7 3.141-7 7v1h17z'></path></svg> Nom";

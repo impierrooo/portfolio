d6a3e99705734e46baaf82580f07a6ba70ad33dc
Pierre Montaret <pierre.montaret@epitech.eu>
2024-05-08 17:46:16 +0200

    [FIX] all text & traduction & image about & resize

--- source/js/langue.js
+++ source/js/langue.js
@@ -24,17 +24,19 @@ function changement_langue(){
             document.getElementById('text2').innerHTML = "Creative and ambitious developer.";
             document.getElementById('text3').innerHTML = "My tools & skills";
             document.getElementById('text4').innerHTML = "With over three years of experience in the field of computer development, I am a passionate creative. My expertise ranges from web development to programming in C, as well as mobile application design. I appreciate every opportunity to apply my skills to create innovative and effective solutions.";
-            document.getElementById('text5').innerHTML = "Zelter is an immersive survival RPG that plunges you into a post-apocalyptic world overrun by zombies. As a survivor, your goal is to rebuild a secure shelter, gather essential resources, and fight off hordes of the undead. With innovative gameplay mechanics and an immersive atmosphere, Zelter delivers an exciting survival experience where every decision matters.";
+            document.getElementById('text5').innerHTML = "Redcode Corewar is a variant of the original Corewar where programs, called champions, are written in Redcode language. Champions are loaded into a virtual arena where they battle for control of memory. Players must design warriors capable of surviving and prevailing in a competitive environment using advanced strategies and creative programming techniques.";
             document.getElementById('index_button').innerHTML = "DISCOVER THE PROJECT";
         }
 
         if (number === 2) {
             document.getElementById('index_header_text1').innerHTML = "PROJECTS";
 
-            document.getElementById('data1').innerHTML = "Language : Lorem ipsum";
-            document.getElementById('data2').innerHTML = "Duration : Lorem ipsum";
-            document.getElementById('data3').innerHTML = "Role : Lorem ipsum";
-            document.getElementById('data4').innerHTML = 'Visualization : <a class="slide_box_link" href="https://loremipsum.io/" target="_blank">Lorem ipsum</a>';
+            document.getElementById('description1').innerHTML = "Redcode Corewar is a variant of the original Corewar where programs, called champions, are written in the Redcode language. These champions are loaded into a virtual arena where they battle for control of memory. Players must design champions capable of surviving and winning in a competitive environment by utilizing advanced strategies and creative programming techniques.";
+
+            document.getElementById('data1').innerHTML = "Language : C";
+            document.getElementById('data2').innerHTML = "Duration : 4 weeks";
+            document.getElementById('data3').innerHTML = "Role : Developer";
+            document.getElementById('data4').innerHTML = 'Visualization : <a class="slide_box_link" href="https://github.com/" target="_blank">Github</a>';
 
             document.getElementById('data5').innerHTML = "Language : Lorem ipsum";
             document.getElementById('data6').innerHTML = "Duration : Lorem ipsum";
@@ -66,17 +68,28 @@ function changement_langue(){
 
             document.getElementById('quote').innerHTML = "In programming, every line is an opportunity to create something extraordinary.";
 
-            document.getElementById('title1').innerHTML = "TITLE";
-            document.getElementById('title2').innerHTML = "TITLE";
-            document.getElementById('title3').innerHTML = "TITLE";
-            document.getElementById('title4').innerHTML = "TITLE";
-            document.getElementById('title5').innerHTML = "TITLE";
+            document.getElementById('text1').innerHTML = "I am Pierre Montaret, and I am seeking a 4 to 6-month internship starting from July 2024 to enhance my journey. Currently undergoing training at Epitech in Rennes, I am passionate about information technology and software development. My academic journey is marked by stimulating projects, such as reproducing commands in C and dynamic graphical projects, demonstrating my ability to tackle technical challenges with creativity and rigor.";
+            document.getElementById('text2').innerHTML = "Outside of my studies, I have developed an interest in investment and cryptocurrencies, showcasing my willingness to explore new fields and tackle complex challenges. My involvement in diverse activities, such as horseback riding and video games, highlights my balance between work and leisure, as well as my ability to effectively manage my time and resources.";
+            document.getElementById('text3').innerHTML = "With the acquisition of my driver's license in 2023, I have gained autonomy that is also reflected in my soft skills, such as creativity, adaptability, and teamwork. These transferable skills, combined with my expertise in computer science and my passion for intellectual challenges, make me an ideal candidate to contribute meaningfully to any project I engage in.";
+            document.getElementById('text4').innerHTML = "As I shared my passions and hobbies, my journey took shape progressively. From my childhood to my teenage years, each step contributed to shaping my path toward the future.";
 
-            document.getElementById('date1').innerHTML = "May 2024";
-            document.getElementById('date2').innerHTML = "May 2024";
-            document.getElementById('date3').innerHTML = "May 2024";
-            document.getElementById('date4').innerHTML = "May 2024";
-            document.getElementById('date5').innerHTML = "May 2024";
+            document.getElementById('title1').innerHTML = "My first computer";
+            document.getElementById('title2').innerHTML = "Discovery of programming";
+            document.getElementById('title3').innerHTML = "My specialization in NSI";
+            document.getElementById('title4').innerHTML = "My beginnings in web development";
+            document.getElementById('title5').innerHTML = "My journey at Epitech";
+
+            document.getElementById('description1').innerHTML = "At the age of 11, I acquired my first computer, marking the beginning of a fascination with the digital world.";
+            document.getElementById('description2').innerHTML = "In second grade, I had my first encounter with programming, a revelation that sparked a passion for coding and algorithms.";
+            document.getElementById('description3').innerHTML = "In high school, I chose the NSI specialty, thus consolidating my knowledge in computer science and laying the groundwork for my future in the technology field.";
+            document.getElementById('description4').innerHTML = "As an independent, I began creating websites, exploring my creativity and honing my technical skills to bring my ideas to life online.";
+            document.getElementById('description5').innerHTML = "Joining Epitech, the school of computer science, was a crucial step in my journey, providing me with specialized training and opening doors to an exciting career in technology.";
+
+            document.getElementById('date1').innerHTML = "December 2016";
+            document.getElementById('date2').innerHTML = "March 2021";
+            document.getElementById('date3').innerHTML = "September 2021";
+            document.getElementById('date4').innerHTML = "February 2022";
+            document.getElementById('date5').innerHTML = "September 2023";
 
             document.getElementById('last1').innerHTML = 'If you appreciate my creations and journey, you can follow me on';
             document.getElementById('last2').innerHTML = 'and';
@@ -113,17 +126,20 @@ function changement_langue(){
             document.getElementById('text2').innerHTML = "Développeur créatif et ambitieux.";
             document.getElementById('text3').innerHTML = "Mes tools & compétences";
             document.getElementById('text4').innerHTML = "Fort d'une expérience de plus de trois ans dans le domaine du développement informatique, je suis un créatif passionné. Mon expertise s'étend du développement web à la programmation en C, en passant par la conception d'applications mobiles. J'apprécie chaque opportunité de mettre en œuvre mes compétences pour créer des solutions innovantes et efficaces.";
-            document.getElementById('text5').innerHTML = "Zelter est un RPG de survie immersif qui vous plonge dans un monde post-apocalyptique infesté de zombies. En tant que survivant, votre objectif est de reconstruire un refuge sécurisé, de collecter des ressources essentielles et de combattre les hordes de morts-vivants. Avec des mécaniques de jeu innovantes et une atmosphère immersive, Zelter offre une expérience de survie palpitante où chaque décision compte.";
+            document.getElementById('text5').innerHTML = "Le Redcode Corewar est une variante du Corewar original où les programmes, appelés champions, sont écrits en langage Redcode. Les champions sont chargés dans une arène virtuelle où ils s'affrontent pour le contrôle de la mémoire. Les joueurs doivent concevoir des guerriers capables de survivre et de vaincre dans un environnement compétitif en utilisant des stratégies avancées et des techniques de programmation créatives.";
             document.getElementById('index_button').innerHTML = "DÉCOUVRIR LE PROJET";
         }
 
         if (number === 2) {
             document.getElementById('index_header_text1').innerHTML = "PROJECT";
 
-            document.getElementById('data1').innerHTML = "Langage : Lorem ipsum";
-            document.getElementById('data2').innerHTML = "Durée : Lorem ipsum";
-            document.getElementById('data3').innerHTML = "Rôle : Lorem ipsum";
-            document.getElementById('data4').innerHTML = 'Visualisation : <a class="slide_box_link" href="https://loremipsum.io/" target="_blank">Lorem ipsum</a>';
+            document.getElementById('description1').innerHTML = "Le Redcode Corewar est une variante du Corewar original où les programmes, appelés champions, sont écrits en langage Redcode. Les champions sont chargés dans une arène virtuelle où ils s'affrontent pour le contrôle de la mémoire. Les joueurs doivent concevoir des champions capables de survivre et de vaincre dans un environnement compétitif en utilisant des stratégies avancées et des techniques de programmation créatives.";
+
+
+            document.getElementById('data1').innerHTML = "Langage : C";
+            document.getElementById('data2').innerHTML = "Durée : 4 semaines";
+            document.getElementById('data3').innerHTML = "Rôle : Développeur";
+            document.getElementById('data4').innerHTML = 'Visualisation : <a class="slide_box_link" href="https://github.com/" target="_blank">Github</a>';
 
             document.getElementById('data5').innerHTML = "Langage : Lorem ipsum";
             document.getElementById('data6').innerHTML = "Durée : Lorem ipsum";
@@ -155,17 +171,28 @@ function changement_langue(){
 
             document.getElementById('quote').innerHTML = "En programmation, chaque ligne est une opportunité de créer quelque chose d'extraordinaire.";
 
-            document.getElementById('title1').innerHTML = "TITRE";
-            document.getElementById('title2').innerHTML = "TITRE";
-            document.getElementById('title3').innerHTML = "TITRE";
-            document.getElementById('title4').innerHTML = "TITRE";
-            document.getElementById('title5').innerHTML = "TITRE";
+            document.getElementById('text1').innerHTML = "Je suis Pierre Montaret, et je suis à la recherche d'un stage de 4 à 6 mois à partir de juillet 2024 pour enrichir mon parcours. Actuellement en formation à Epitech à Rennes, je me passionne pour les technologies de l'information et la conception de logiciels. Mon parcours académique est marqué par des projets stimulants, comme la reproduction de commandes en C et des projets graphiques dynamiques, démontrant ma capacité à relever des défis techniques avec créativité et rigueur.";
+            document.getElementById('text2').innerHTML = "En dehors de mes études, j'ai développé un intérêt pour l'investissement et les cryptomonnaies, illustrant ma volonté d'explorer de nouveaux domaines et de relever des défis complexes. Mon engagement dans des activités variées, telles que l'équitation et les jeux vidéo, met en lumière mon équilibre entre le travail et les loisirs, ainsi que ma capacité à gérer efficacement mon temps et mes ressources.";
+            document.getElementById('text3').innerHTML = "Avec l'obtention de mon permis de conduire en 2023, j'ai acquis une autonomie qui se reflète également dans mes soft skills, telles que la créativité, l'adaptabilité et le travail en équipe. Ces compétences transversales, combinées à mon expertise en informatique et à ma passion pour les défis intellectuels, font de moi un candidat idéal pour contribuer de manière significative à tout projet dans lequel je m'engage.";
+            document.getElementById('text4').innerHTML = "Alors que je partageais mes passions, mon parcours prenait forme de manière progressive. De mon enfance à mes années d'adolescence, chaque étape contribuait à forger mon chemin vers l'avenir.";
 
-            document.getElementById('date1').innerHTML = "Mai 2024";
-            document.getElementById('date2').innerHTML = "Mai 2024";
-            document.getElementById('date3').innerHTML = "Mai 2024";
-            document.getElementById('date4').innerHTML = "Mai 2024";
-            document.getElementById('date5').innerHTML = "Mai 2024";
+            document.getElementById('title1').innerHTML = "Mon premier ordinateur";
+            document.getElementById('title2').innerHTML = "Découverte de la programmation";
+            document.getElementById('title3').innerHTML = "Ma spécialisation en NSI";
+            document.getElementById('title4').innerHTML = "Mes débuts en création web";
+            document.getElementById('title5').innerHTML = "Mon parcours à Epitech";
+
+            document.getElementById('description1').innerHTML = "À l'âge de 11 ans, j'ai fait l'acquisition de mon premier ordinateur, marquant le début d'une fascination pour le monde numérique.";
+            document.getElementById('description2').innerHTML = "En seconde, j'ai eu ma première rencontre avec la programmation, une révélation qui a déclenché une passion pour le codage et les algorithmes.";
+            document.getElementById('description3').innerHTML = "Au lycée, j'ai choisi la spécialité NSI, consolidant ainsi mes connaissances informatiques et préparant le terrain pour mon avenir dans le domaine de la technologie.";
+            document.getElementById('description4').innerHTML = "En tant qu'indépendant, j'ai commencé à créer des sites web, explorant ma créativité et développant mes compétences techniques pour donner vie à mes idées en ligne.";
+            document.getElementById('description5').innerHTML = "Intégrer Epitech, l'école d'informatique, a été une étape cruciale dans mon parcours, me fournissant une formation spécialisée et ouvrant les portes à une carrière passionnante dans la technologie.";
+
+            document.getElementById('date1').innerHTML = "Décember 2016";
+            document.getElementById('date2').innerHTML = "Mars 2021";
+            document.getElementById('date3').innerHTML = "Septembre 2021";
+            document.getElementById('date4').innerHTML = "Février 2022";
+            document.getElementById('date5').innerHTML = "Septembre 2023";
 
             document.getElementById('last1').innerHTML = 'Si vous appréciez mes créations et mon parcours, vous pouvez me suivre sur';
             document.getElementById('last2').innerHTML = 'et';

6bf2bd87b29d55dc7528ef30b7f95b936446db27
impierrooo <montaret.pierre@yahoo.com>
2024-07-30 23:36:32 +0200

    remove all

--- source/js/change_css.js
+++
@@ -1,9 +0,0 @@
-function change_css() {
-    var elements = document.getElementsByClassName("container");
-    for (var i = 0; i < elements.length; i++) {
-        if (elements[i].style.zIndex === '1001')
-            elements[i].style.zIndex = '100';
-        else
-            elements[i].style.zIndex = '1001';
-    }
-}
